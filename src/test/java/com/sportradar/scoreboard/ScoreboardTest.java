9932b64263daece60cc9285ca9b63f11eb1ba5cb
Yuliya Kalcheva <hulyakurt89@hotmail.com>
2026-03-25 23:41:45 +0100

    Add summary example and finish-match tests

--- src/test/java/com/sportradar/scoreboard/ScoreboardTest.java
+++ src/test/java/com/sportradar/scoreboard/ScoreboardTest.java
@@ -83,4 +83,47 @@ class ScoreboardTest {
         assertEquals("Spain", summary.get(0).getHomeTeam());
         assertEquals("Brazil", summary.get(0).getAwayTeam());
     }
+
+    @Test
+    void shouldReturnSummaryAsInExample() {
+        Scoreboard scoreboard = new Scoreboard();
+
+        scoreboard.startMatch("Mexico", "Canada");
+        scoreboard.startMatch("Spain", "Brazil");
+        scoreboard.startMatch("Germany", "France");
+        scoreboard.startMatch("Uruguay", "Italy");
+        scoreboard.startMatch("Argentina", "Australia");
+
+        scoreboard.updateScore("Mexico", "Canada", 0, 5);
+        scoreboard.updateScore("Spain", "Brazil", 10, 2);
+        scoreboard.updateScore("Germany", "France", 2, 2);
+        scoreboard.updateScore("Uruguay", "Italy", 6, 6);
+        scoreboard.updateScore("Argentina", "Australia", 3, 1);
+
+        List<Match> summary = scoreboard.getSummary();
+
+        assertEquals("Uruguay", summary.get(0).getHomeTeam());
+        assertEquals("Spain", summary.get(1).getHomeTeam());
+        assertEquals("Mexico", summary.get(2).getHomeTeam());
+        assertEquals("Argentina", summary.get(3).getHomeTeam());
+        assertEquals("Germany", summary.get(4).getHomeTeam());
+    }
+
+    @Test
+    void shouldNotIncludeFinishedMatchInSummary() {
+        Scoreboard scoreboard = new Scoreboard();
+
+        scoreboard.startMatch("Mexico", "Canada");
+        scoreboard.startMatch("Spain", "Brazil");
+
+        scoreboard.updateScore("Mexico", "Canada", 0, 5);
+        scoreboard.updateScore("Spain", "Brazil", 3, 2);
+
+        scoreboard.finishMatch("Mexico", "Canada");
+
+        List<Match> summary = scoreboard.getSummary();
+
+        assertEquals(1, summary.size());
+        assertEquals("Spain", summary.get(0).getHomeTeam());
+    }
 }
